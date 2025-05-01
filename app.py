1da9c1c912ad1dc1144c888fff3c0d289ecdba8f
Lyching Kong <lychingkong@gmail.com>
2025-05-01 13:40:44 +0700

    5th commit

--- app.py
+++ app.py
@@ -86,5 +86,5 @@ def laundry_details(city):
     return render_template("laundry.html", city=city.title(), forecast=time_precip, tip=tip)
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0',debug=True)
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
+    # app.run(debug=True)
